25fcb0fff50938a360b6a0bdeea5f73c987dca03
Ori <38721999+OriSpirit@users.noreply.github.com>
2022-05-28 19:28:46 +0800

    fix init fm npe

--- src/main/java/com/spiritlight/rainstorm/events/FeatureManager.java
+++ src/main/java/com/spiritlight/rainstorm/events/FeatureManager.java
@@ -3,20 +3,12 @@ package com.spiritlight.rainstorm.events;
 import com.spiritlight.rainstorm.features.*;
 
 public class FeatureManager {
-    public static Blink blink;
-    public static BlockTP blockTP;
-    public static EntityVelocity entityVelocity;
-    public static Fly fly;
-    public static GhostBlock ghostBlock;
-    public static NoFall noFall;
-    public static Spider spider;
-    private FeatureManager() {
-        blink = new Blink();
-        blockTP = new BlockTP();
-        entityVelocity = new EntityVelocity();
-        fly = new Fly();
-        ghostBlock = new GhostBlock();
-        noFall = new NoFall();
-        spider = new Spider();
-    }
+    public static Blink blink = new Blink();;
+    public static BlockTP blockTP = new BlockTP();
+    public static EntityVelocity entityVelocity = new EntityVelocity();
+    public static Fly fly = new Fly();
+    public static GhostBlock ghostBlock = new GhostBlock();
+    public static NoFall noFall = new NoFall();
+    public static Spider spider = new Spider();
+    private FeatureManager() {}
 }
